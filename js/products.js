6d8e7c5803f7fa28d944aca79cfb596286683f86
ilearnbydoing <dgweb123@gmail.com>
2019-02-05 09:27:57 +0530

    first commit

---
+++ js/products.js
@@ -0,0 +1,57 @@
+var $products,
+
+	$current_product = 'tourpress';
+
+
+
+// List all the products here
+
+
+
+$products = {
+
+		tourpress : {
+
+		name     : 'Tour Press',
+
+		tag      : 'WP',
+
+		img      : 'images/tourpress.png',
+
+		url      : 'http://tourpress.inspirydemos.com/',
+
+		purchase : 'https://www.webanaya.com/contact',
+
+	},
+
+	marketo : {
+
+	name     : 'Marketo',
+
+	tag      : 'WP',
+
+	img      : 'images/marketo.jpg',
+
+	url      : 'https://wp.xpeedstudio.com/marketo/',
+
+	purchase : 'https://www.webanaya.com/contact',
+
+},
+instikit : {
+
+name     : 'IMS',
+
+tag      : 'PHP',
+
+img      : 'images/marketo.jpg',
+
+url      : 'https://demo.instikit.com/dashboard',
+
+purchase : 'https://www.webanaya.com/contact',
+
+},
+
+
+
+
+};
